f8565b4219442ba289824dc2d3ca3ffb7c3faa39
paxx <asherpoquit@gmail.com>
2025-03-09 22:01:57 +0800

    oki

--- Security/Security/src/main/java/config/SecurityConfig.java
+++ Security/Security/src/main/java/config/SecurityConfig.java
@@ -11,14 +11,18 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
     @Bean
-    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain defaultSecurityChain(HttpSecurity http) throws Exception {
         return http
-                .authorizeHttpRequests(authorizeRequests ->
-                        authorizeRequests.anyRequest().authenticated())
-                .oauth2Login(oauth2 -> oauth2.defaultSuccessUrl("http://localhost:8080/user-info", true))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/", "/contacts", "/api/contacts").permitAll()  // Allow unauthenticated access
+                        .anyRequest().authenticated()
+                )
+                .oauth2Login(oauth -> oauth.defaultSuccessUrl("/contacts", true))
                 .logout(logout -> logout.logoutSuccessUrl("/"))
                 .formLogin(form -> form.defaultSuccessUrl("/secured", true))
                 .csrf(AbstractHttpConfigurer::disable)
                 .build();
     }
+
 }
+
